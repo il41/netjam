4074bacbcffd694a7140ad62f454f793e73e8d2c
il41 <igilbe1@artic.edu>
2018-11-21 22:08:30 -0600

    added buttonArr2 in matrix2

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,9 @@
+let scale1 = ['D3','F3','G3','A3','Bb3','C4','D4','Eb4','F4','G4','A4','Bb4','C5','D5','Eb5','F5','G5','A5','Bb5','D6']
 let cMaj = ['C3','D3','E3','F3','G3','A4','B4','C4','D4','E4','G4']
 let cMin = ['C3','D3','Eb3','F3','G3','Ab4','Bb4','C4','D4','Eb4','G4']
 let fivecircle = ['G3','C3','G4','D3','A4','E4','B5','F#5','C#6','G#6','D#6','A#7','F7','C7']
 let fourcircle = ['C3','F4','Bb5','Eb5','Ab6','Db6','Gb6','B7','E7','A7']
+let newscale1 = ['Eb3','Bb4','D4','A4','F5']
 // here we create a socket variable (our connection to the server) using the
 // socket.io library (included in the index.html page)
 const socket = io()
@@ -9,8 +11,8 @@ const socket = io()
 const enter = document.querySelector('#enter')
 const userscaletxt = document.querySelector('#userscaletext')
 const userscalename = document.querySelector('#userscalename')
-
-let scale = ['D3','F3','G3','A3','Bb3','C4','D4','Eb4','F4','G4','A4','Bb4','C5','D5','Eb5','F5','G5','A5','Bb5','D6']
+let scale = newscale1
+//let scale = ['D3','F3','G3','A3','Bb3','C4','D4','Eb4','F4','G4','A4','Bb4','C5','D5','Eb5','F5','G5','A5','Bb5','D6']
 var scaleSelect = document.getElementById("scaleSelect");
 //scaleSelect.value = scale
 
@@ -29,6 +31,8 @@ inputnode.connect(delayDry)
 
 let matrix = document.createElement("matrixdiv")
 document.body.appendChild(matrix)
+let matrix2 = document.createElement("matrixdiv2")
+document.body.appendChild(matrix2)
 
 let buttonArr = []
 for(let i = 0; i < 80; i++){
@@ -36,6 +40,14 @@ for(let i = 0; i < 80; i++){
   let parent = matrix
   let b = new SineButton(scale[idx],parent,inputnode)
   buttonArr.push(b)
+}
+let buttonArr2 = []
+for(let i = 0; i < 80; i++){
+  scale = scale1
+  let idx = i % scale.length
+  let parent = matrix2
+  let b2 = new SineButton(scale1[idx],parent,inputnode)
+  buttonArr.push(b2)
 }
 
 let userVolume = document.body.querySelector("#userVolume")
