0ca9f5025969c675c609aef1f39d605664bcf49e
il41 <igilbe1@artic.edu>
2018-11-28 11:13:34 -0600

    fixed local scale switcher

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,11 @@
-let scale1 = ['D3','F3','G3','A3','Bb3','C4','D4','Eb4','F4','G4','A4','Bb4','C5','D5','Eb5','F5','G5','A5','Bb5','D6']
-let cMaj = ['C3','D3','E3','F3','G3','A4','B4','C4','D4','E4','G4']
-let cMin = ['C3','D3','Eb3','F3','G3','Ab4','Bb4','C4','D4','Eb4','G4']
-let fivecircle = ['G3','C3','G4','D3','A4','E4','B5','F#5','C#6','G#6','D#6','A#7','F7','C7']
-let fourcircle = ['C3','F4','Bb5','Eb5','Ab6','Db6','Gb6','B7','E7','A7']
-let newscale1 = ['Eb3','Bb4','D4','A4','F5']
+const scales = {
+  sad : ['Eb3','Bb4','D4','A4','F5'],
+  sad2 : ['D3','F3','G3','A3','Bb3','C4','D4','Eb4','F4','G4','A4','Bb4','C5','D5','Eb5','F5','G5','A5','Bb5','D6'],
+  cMaj : ['C3','D3','E3','F3','G3','A4','B4','C4','D4','E4','G4'],
+  cMin : ['C3','D3','Eb3','F3','G3','Ab4','Bb4','C4','D4','Eb4','G4'],
+  fivecircle : ['G3','C3','G4','D3','A4','E4','B5','F#5','C#6','G#6','D#6','A#7','F7','C7'],
+  fourcircle : ['C3','F4','Bb5','Eb5','Ab6','Db6','Gb6','B7','E7','A7'],
+}
 // here we create a socket variable (our connection to the server) using the
 // socket.io library (included in the index.html page)
 const socket = io()
@@ -11,8 +13,8 @@ const socket = io()
 const enter = document.querySelector('#enter')
 const userscaletxt = document.querySelector('#userscaletext')
 const userscalename = document.querySelector('#userscalename')
-let scale = newscale1
-//let scale = ['D3','F3','G3','A3','Bb3','C4','D4','Eb4','F4','G4','A4','Bb4','C5','D5','Eb5','F5','G5','A5','Bb5','D6']
+let scale = scales["sad"]
+
 var scaleSelect = document.getElementById("scaleSelect");
 //scaleSelect.value = scale
 
@@ -30,7 +32,8 @@ inputnode.connect(delay)
 inputnode.connect(delayDry)
 
 
-let matrix = document.createElement("matrixdiv")
+let matrix = document.createElement("div")
+matrix.className = "matrixdiv"
 document.body.appendChild(matrix)
 // let matrix2 = document.createElement("matrixdiv2")
 // document.body.appendChild(matrix2)
@@ -38,36 +41,31 @@ document.body.appendChild(matrix)
 let buttonArr = []
 for(let i = 0; i < 80; i++){
   let idx = i % scale.length
-  let parent = matrix
-  let b = new SineButton(scale[idx],parent,inputnode)
+  let b = new SineButton(scale[idx],matrix,inputnode)
   buttonArr.push(b)
 }
 // let buttonArr2 = []
 // for(let i = 0; i < 80; i++){
-//   scale = scale1
+//   scale = scales["sad2"]
 //   let idx = i % scale.length
 //   let parent = matrix2
-//   let b2 = new SineButton(scale1[idx],parent,inputnode)
+//   let b2 = new SineButton(scale[idx],parent,inputnode)
 //   buttonArr.push(b2)
 // }
 
 function matrixScaleChange(){
-  //Destroy matrixdiv and reset buttonArr
+  //Destroy matrix div and reset buttonArr
   document.body.removeChild(matrix)
+  buttonArr = []
+
+  //Make new div and repopulate buttonArr with new scale
+  matrix = document.createElement("div")
+  matrix.className = "matrixdiv"
+  document.body.appendChild(matrix)
   for(let i = 0; i < 80; i++){
+    scale = scales[scaleSelect.value]
     let idx = i % scale.length
-    let parent = matrix
-    let b = new SineButton(scale[idx],parent,inputnode)
-    buttonArr.pop(b)
-  }
-  //Repopulate buttonArr with new scale
-  let matrix = document.createElement("matrixdiv")
-  for(let i = 0; i < 80; i++){
-    //how do i get the scale value string to refrence the variable
-    scale = scaleSelect.value
-    let idx = i % scale.length
-    let parent = matrix
-    let b = new SineButton(scale[idx],parent,inputnode)
+    let b = new SineButton(scale[idx],matrix,inputnode)
     buttonArr.push(b)
   }
 }
